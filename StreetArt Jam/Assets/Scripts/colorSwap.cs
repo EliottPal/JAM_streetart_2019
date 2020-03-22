987b2f93ad316b218ebf0f8084f1338e352af318
Eliott Pal <eliott.^palueau@epitech.eu>
2020-03-22 13:54:19 +0100

    Adding color button with B

---
+++ StreetArt Jam/Assets/Scripts/colorSwap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorSwap : MonoBehaviour
+{
+    public Sprite blue;
+    public Sprite green;
+
+    bool isActive;
+
+    void Start()
+    {
+        isActive = false;
+        this.GetComponent<SpriteRenderer>().sprite = blue;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.B)) {
+            if (isActive == true) {
+                isActive = false;
+                this.GetComponent<SpriteRenderer>().sprite = blue;
+                Debug.Log("Blue spray");
+            }
+            else if (isActive == false) {
+                isActive = true;
+                this.GetComponent<SpriteRenderer>().sprite = green;
+                Debug.Log("Green spray");
+            }
+        }
+    }
+}
