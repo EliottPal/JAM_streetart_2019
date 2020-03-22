808b8e3bbb0c64cdea187c8a200d7b14f9dc5608
Assurio <meven.dugor@epitech.eu>
2020-03-22 20:48:31 +0100

    begin explosion

---
+++ StreetArt Jam/Assets/Scripts/ExplosionScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour
+{
+    public Animator animator;
+    public BoxCollider2D boxCollider2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        CharacController controller = other.GetComponent<CharacController>();
+
+        if (controller != null)
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
