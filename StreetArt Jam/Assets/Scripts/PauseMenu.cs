8467cfbd96bb4ce9f93b5acf8f3fde686771b1a9
rodzer <rodolphe.dupuis@epitech.eu>
2020-03-22 16:27:42 +0100

    adding pause menu display :: need to create buttons

---
+++ StreetArt Jam/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    bool isPaused = false;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape) | Input.GetKeyDown(KeyCode.P)) {
+            if (isPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+}
