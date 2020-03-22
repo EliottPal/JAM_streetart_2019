66555e2af3428557c3e9212b3242dcdec5149a0b
rodzer <rodolphe.dupuis@epitech.eu>
2020-03-22 13:38:08 +0100

    update architecture toggle fullscreen (still not working)

---
+++ StreetArt Jam/Assets/ManageScenes.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageScenes : MonoBehaviour
+{
+    public void ToggleFullScreen(bool isFullScreen) {
+        Screen.fullScreen = !Screen.fullScreen;
+        Debug.Log("FULLSCREEN MODE: " + Screen.fullScreen);
+    }
+}
