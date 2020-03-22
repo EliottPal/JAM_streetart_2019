5d37d55833def2185b1d06cb8a66e921c84722a9
rodzer <rodolphe.dupuis@epitech.eu>
2020-03-22 15:04:00 +0100

    adding level selector through menu scene

--- StreetArt Jam/Assets/MainMenu.cs
+++ StreetArt Jam/Assets/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
 
     public void PlayGame() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("LevelSelector");
     }
 
     public void QuitGame() {
---
+++ StreetArt Jam/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public void LoadLevelOne() {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+}
