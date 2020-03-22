f4ff24b5362e43a3cf8685991188074286628a12
Eliott Pal <eliott.^palueau@epitech.eu>
2020-03-22 12:08:44 +0100

    Adding spray UI toggle and sound

---
+++ StreetArt Jam/Assets/Scripts/SprayUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+public class SprayUI : MonoBehaviour
+{
+    public Sprite active;
+    public Sprite inactive;
+    public AudioSource spray;
+    bool isActive;
+
+    void Start()
+    {
+        isActive = false;
+        this.GetComponent<SpriteRenderer>().sprite = inactive;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.A)) {
+            if (isActive == true) {
+                isActive = false;
+                this.GetComponent<SpriteRenderer>().sprite = inactive;
+                Debug.Log("Now inactive");
+            }
+            else if (isActive == false) {
+                isActive = true;
+                this.GetComponent<SpriteRenderer>().sprite = active;
+                spray.Play();
+                Debug.Log("Now Active");
+            }
+        }
+    }
+}
